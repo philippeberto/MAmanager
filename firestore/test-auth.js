411c969658bee04d7dc567f3301bb222c6e23512
Philippe <philippe.bjj@gmail.com>
2020-08-08 16:48:15 +0100

    desacoplamento front-end e back-end

--- firestore/test-auth.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("../firestore.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gym-bd.firebaseio.com"
-});
