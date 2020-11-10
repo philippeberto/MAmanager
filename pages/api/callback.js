9b9d0b249efb10b7a5873005ca09ca57286c2ad1
Philippe <philippe.bjj@gmail.com>
2020-11-10 12:11:19 +0000

    teste api/callback.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,7 +1,8 @@
 import auth0 from '../../lib/auth0'
 
 const callback = async (req, res) => {
-  await auth0.handleCallback(req, res, { redirectTo: '/usuario' })
+  console.log(req)
+  await auth0.handleCallback(req, res, { redirectTo: '/' })
 }
 
 export default callback
