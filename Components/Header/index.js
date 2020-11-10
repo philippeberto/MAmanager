13db264d8e2a1bde43681deb6c33c1aecd723109
Philippe <philippe.bjj@gmail.com>
2020-11-10 09:00:59 +0000

    Update index.js
    correção das urls

--- Components/Header/index.js
+++ Components/Header/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Link from 'next/link'
-import { useState } from 'react'
-import auth0 from '../../lib/auth0'
 import Image from 'next/image'
 
 const Header = () => {
@@ -12,10 +10,10 @@ const Header = () => {
           <Image src='/img/GBlogo.png' width='210' height='45' className="logo" />
         </a>
       </Link>
-      <Link href="api/login">
+      <Link href="/api/login">
         <a className="login">Entrar</a>
       </Link>|
-      <Link href="api/logout">
+      <Link href="/api/logout">
         <a className="logout"> Sair</a>
       </Link>
     </div>
