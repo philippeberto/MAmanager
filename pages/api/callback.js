01c9348a66cb9f341aee899ed5d1b69e1fec4bc0
Philippe <philippe.bjj@gmail.com>
2020-11-10 09:20:50 +0000

    update login.js

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,7 +1,7 @@
 import auth0 from '../../lib/auth0'
 
 const callback = async (req, res) => {
-  await auth0.handleCallback(req, res, { redirectTo: '/' })
+  await auth0.handleCallback(req, res, { redirectTo: '/usuario' })
 }
 
 export default callback
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,8 @@
 import auth0 from '../../lib/auth0'
 
 const login = async (req, res) => {
-  await auth0.handleLogin(req, res)
+  try { await auth0.handleLogin(req, res) }
+  catch (err) { }
 }
 
 export default login
